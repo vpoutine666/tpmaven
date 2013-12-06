009b82913a9e05cecff0ceb1632969e436c7dee9
Langlais Jonathan <jlanglai@roi.educ.insa>
2013-12-06 11:36:42 +0100

    first commit

---
+++ src/main/java/fr/insa/rennes/App.java
@@ -0,0 +1,21 @@
+package fr.insa.rennes;
+
+/**
+ * Hello world!
+ * Pui de fefere este foarte caraghios
+ * 
+ * @author jlanglai
+ * @version 1.0
+ *
+ */
+public class App 
+{
+	/**
+	 * Main de l'application
+	 * @param args argument passe a la commande
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
